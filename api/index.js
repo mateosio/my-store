264b7fdeab8a01202b08c0fee4d935b804076a7d
Mateo Sio <siomateo8@gmail.com>
2023-11-28 19:45:42 -0300

    cambios en el index.js para ver si funciona en versel

--- api/index.js
+++ api/index.js
@@ -1,47 +1,97 @@
 const express = require("express");
 const routerApi = require("./routes");
-const {logErrors, errorHandler, boomErrorHandler, ormErrorHandler} = require("./middlewares/error.handlers");
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require("./middlewares/error.handlers");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const list = ["http://localhost:8080", "https://myapp.co"];
+const options = {
+  origin: (origin, callback) => {
+    if (list.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido"));
+    }
+  }
+};
 
-// app.get("/nueva-ruta", (req, res)=>{
-  //   res.json({
-    //     producto: "Puerta"
-    //   })
-    // });
+app.use(cors(options));
 
-    const list = ["http://localhost:8080", "https://myapp.co"];
-    const options = {
-      origin: (origin, callback) => {
-        if(list.includes(origin) || !origin){
-          callback(null, true);
-        } else{
-          callback(new Error("No permitido"));
-        }
-      }
-    };
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal, sirve frontend.html desde la carpeta "api"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "api", "frontend.html"));
+});
+
+// Ruta de la API
+app.get("/api", (req, res) => {
+  res.send("Hola server");
+});
+
+// Rutas adicionales de la API
+routerApi(app);
+
+// Middlewares de manejo de errores
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// const express = require("express");
+// const routerApi = require("./routes");
+// const {logErrors, errorHandler, boomErrorHandler, ormErrorHandler} = require("./middlewares/error.handlers");
+// const cors = require("cors");
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.json());
 
 
-    app.use(cors(options));
+// // app.get("/nueva-ruta", (req, res)=>{
+//   //   res.json({
+//     //     producto: "Puerta"
+//     //   })
+//     // });
 
-    app.get("/api", (req, res)=>{
-      res.send("Hola server")
-    });
+//     const list = ["http://localhost:8080", "https://myapp.co"];
+//     const options = {
+//       origin: (origin, callback) => {
+//         if(list.includes(origin) || !origin){
+//           callback(null, true);
+//         } else{
+//           callback(new Error("No permitido"));
+//         }
+//       }
+//     };
 
-    routerApi(app);
 
-    app.use(logErrors);
-    app.use(ormErrorHandler);
-    app.use(boomErrorHandler);
-    app.use(errorHandler);
+//     app.use(cors(options));
 
-    app.listen(port, ()=>{
-      console.log("Server");
-    });
+//     app.get("/api", (req, res)=>{
+//       res.send("Hola server")
+//     });
+
+//     routerApi(app);
+
+//     app.use(logErrors);
+//     app.use(ormErrorHandler);
+//     app.use(boomErrorHandler);
+//     app.use(errorHandler);
+
+//     app.listen(port, ()=>{
+//       console.log("Server");
+//     });
 
 
